1a7c864b4443e68f28cd7fb1deba311e18ed561a
Luka <luka@redkoan.com>
2021-07-23 14:46:05 +0200

    css

--- src/lib/ComponentFactory.js
+++ src/lib/ComponentFactory.js
@@ -11,18 +11,14 @@ export const CT_DIV = 'BoxComponent';
 
 export const componentTypes = [CT_M, CT_H1, CT_H2, CT_P, CT_UL, CT_B, CT_A, CT_DIV];
 
-const H1Component = (p) => {
-    console.log('pppp',p);
-    return (<h1>h1: {p.text}</h1>);
-}
-const BoxComponent = ({children}) => (<div>{children}</div>);
+const BoxComponent = ({children, borderSize = '0px'}) => (<div style={{border: `${borderSize} solid #000`}}>{children}</div>);
+const H1Component = ({text}) => (<h1>{text}</h1>);
 const H2Component = ({text}) => (<h2>h2: {text}</h2>);
 const ParagraphComponent = ({text}) => (<p>{text}</p>);
 const ModalComponent = ({children}) => (<p>{children}</p>);
 const LinkComponent = ({text}) => (<p>{text}</p>);
 const ButtonComponent = ({text}) => (<button>{text}</button>);
-const ListComponent = ({li}) => (<ul>{ li ? li.map(i => <li>{i}</li>) : 'nostuff' }</ul>);
-
+const ListComponent = ({li}) => (<ul>{ li ? li.map((item, index) => <li key={index}>{item}</li>) : null }</ul>);
 
 export const componentsByTypeMap = {
     [CT_M]: ModalComponent,
@@ -44,8 +40,7 @@ const resolveComponentByType = type => {
 
 const ComponentFactory = (config) => {
     const Comp = resolveComponentByType(config.type);
-    console.log('PS',config.props);
-    return () => <Comp { ...config.props } >{config.children}</Comp>
+    return () => <Comp { ...config.props } key={config.key} >{config.children}</Comp>
 };
 
 export default ComponentFactory;
